15f668029cb125edb0a793da53b972a0b144c400
Miguel A. Simeón <miguelsimeon@gmail.com>
2016-11-05 21:43:12 +0100

    PMM - Proyecto 01

---
+++ PMM_P01/app/src/main/java/masimeon/pmm_p01/dam/florida/com/pmm_p01/MainActivity.java
@@ -0,0 +1,21 @@
+package masimeon.pmm_p01.dam.florida.com.pmm_p01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onResume(){
+        super.onResume();
+    }
+
+    protected void onPause(){
+        super.onPause();
+    }
+}
---
+++ PMM_P01/app/src/main/java/masimeon/pmm_p01/dam/florida/com/pmm_p01/SplashActivity.java
@@ -0,0 +1,55 @@
+package masimeon.pmm_p01.dam.florida.com.pmm_p01;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Visibilizamos el splash
+        setContentView(R.layout.activity_splash);
+        // Escondemos la barra de títulos de esta Activity
+        getSupportActionBar().hide();
+
+        //Empezamos con el retraso de inicio de pantalla
+        TimerTask retraso_inicio = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Iniciamos el MainActivity.class
+                //Hacemos una llamada donde indicamos con el .this la activity actual
+                //y con el .class (llamamos a la clase) la quq queremos llamar
+                Intent llamada_app = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(llamada_app);
+
+                //Cerramos esta class/activity para que el usuario no pueda volver a ella
+                finish();
+            }
+        };
+
+        //Una vez creada la tarea que crea el retraso e inicia la siguiente activity
+        //solo nos falta crear un temporizador con el tiempo de retraso
+        Timer temporizador = new Timer();
+        temporizador.schedule(retraso_inicio, 6000); //Aquí le indicamos el tiempo, puede ser interesante sacar este dato en una variable global
+
+    }
+
+    protected void onResume(){
+        super.onResume();
+    }
+
+    protected void onPause(){
+        super.onPause();
+    }
+
+}
